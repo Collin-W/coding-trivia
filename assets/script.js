5d546d893b9f204eb78560f1c2b80528561be1a1
Collin-W <whalencollin@gmail.com>
2021-09-19 19:38:50 -0500

    timer and score feat.

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,10 @@
-//connect timer to start click
-//the highscore must be stored in the local storage then displayed 
-//a correct answer should take you to next question, and a wrong answer deducts 5 seconds
-//if timer ends, quiz is over
-
-//var hotspring
+//var
 var startClick = document.querySelector(".startTrivia");
-var counter = document.querySelector(".counter");
+var counter = document.getElementById("counter");
 var startTimer = 0;
-var timer = 0;
+var timeLeft = 0;
+var timeInterval = 0;
 var questionCount = 0;
-
 
 //dynamic html element var
 var quizContainer;
@@ -20,24 +15,24 @@ var optionBtn;
 
 var quizQuestionsArray = [ 
     {
-        question: "Question #1: Color of the sky",
-        options: ["blue", "green", "grey", "orange"],
-        correct: "blue"
+        question: "Question #1: Which of the following is not a falsy value in JavaScript?",
+        options: ["0", "-0", "null", "never"],
+        correct: "never"
     },
     {
-        question: "Question #2: Color of a banana",
-        options: ["green", "blue", "grey", "yellow"],
-        correct: "yellow"
+        question: "Question #2: When using the confirm() method for user input, what type of data can we expect to return?",
+        options: ["string", "boolean", "integer", "null"],
+        correct: "boolean"
     },
     {
-        question: "Question #3: Color of a grass",
-        options: ["grey", "green", "red", "blue"],
-        correct: "green"
+        question: "Question #3: What is the argument of a function? ",
+        options: ["Similar to semantic html, for accessiblity in JavaScript functions.", "The call triggering the function to execute.", "Arguments pass data into a function.", "A repeated loop that executes when conditions stay true. "],
+        correct: "Arguments pass data into a function."
     },
     {
-        question: "Question #4: Color of a coffee",
-        options: ["blue", "yellow", "brown", "whites"],
-        correct: "brown"
+        question: "Question #4: What does the (this) method refer to in a function?",
+        options: ["(this) is a self referance to an object.", "A semantic variable placeholder method.", "A specific object in an array.", "A boolean with a true or false depending on (this) condition/s."],
+        correct: "(this) is a self referance to an object"
     },
 ];
 
@@ -46,21 +41,23 @@ console.log(quizQuestionsArray[questionCount]);
 //this function should delete old html, and loop to next object in array
 var nextQuestion = function() { 
     questionCount++
+    if (questionCount === quizQuestionsArray.length) {endGame()};
     createQuiz();
 };
 
 var createQuiz = function() {
+
+    
     
 
     startClick.remove();
     console.log("startClick");
 
-    
+   
 
     var quizContainer = document.createElement("div");
     quizContainer.className = "quiz-contents";
     quizContainer.setAttribute("quiz-contents", "");
-    //body.appendChild(quizContainer);
     document.getElementById("body").appendChild(quizContainer); 
 
     var quizQuestion = document.createElement("p");
@@ -82,10 +79,12 @@ var options = quizQuestionsArray[questionCount].options;
             alert("correct");
             quizContainer.remove();
             nextQuestion();
+        } else if( questionCount === quizQuestionsArray.length) {
+            endGame();
         } else {
             alert("wrong");
             quizContainer.remove();
-            timer += -5;
+            timeLeft = timeLeft + -5;
             nextQuestion();
         };
     });
@@ -93,11 +92,39 @@ var options = quizQuestionsArray[questionCount].options;
 };
 
 var endGame = function() {
-    alert("You scored this many points ");
+    clearInterval(timeLeft);
+    alert("Your score is " + timeLeft);
+};
+
+function countdown() {
+timeLeft = 60;
+var timeInterval = setInterval(function(){
+    if (timeLeft <= 1) {
+        quizContainer.remove();
+        alert('Time is up!');
+        clearInterval(timeInterval);
+    };
+      counter.textContent = timeLeft;
+      timeLeft--
+      console.log(timeLeft);
+  }, 1000);
 };
 
 
 startClick.addEventListener("click", createQuiz);
+startClick.addEventListener("click", countdown);
+
+localStorage.setItem("high-score", timeLeft)
+
+var renderScore = function() {
+    var oldScore
+   var newScore = localStorage.getItem("high-score", timeLeft);
+   if (newScore > oldScore) {
+       alert("A new high score! Your new record is " + newScore);
+   } else {
+       alert("The highscore is " + oldScore + ". You did not beat the highscore!");
+   }
+};
 
 
 //setInterval(function, milliseconds, param1, param2, ...)
